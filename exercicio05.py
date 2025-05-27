3cb13e3457bd8e51a647dd898eadb21765c1cb17
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-05-27 12:02:44 +0000

    05

--- exercicio05.py
+++ exercicio05.py
@@ -1,3 +1,5 @@
-consumo_mensal = int(input("Digite o número do consumo de sua internet: "))
-
-if consumo_mensal == consumo_mensal:
+consumo_mensal = int(input("Digite o consumo diário: "))
+if consumo_mensal >= 100 :
+   print("Alerta! Seu consumo ultrapassou os 100GB permitidos.")
+else:
+   print("Consumo adequado.")
